fe3c2835d8751e6b51dea595b4e50aa44e1d3cd4
Sophie LONGO <sophie@donkeycode.com>
2018-08-03 13:05:16 +0200

    doc site wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NgxToolsModule } from 'ngx-tools/ngx-tools';
+import { AppRoutingModule } from './app-routing.module';
+import { NgxToolsModule } from '../../projects/ngx-tools/src/lib/ngx-tools.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     NgxToolsModule,
   ],
