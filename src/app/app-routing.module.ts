93d78416731c82569daa44ac9aef218274e2a220
Sophie LONGO <sophie@donkeycode.com>
2018-08-03 13:51:25 +0200

    doc site

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LayoutComponent } from './layout.component';
+import { HomeComponent } from './home.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: LayoutComponent
+    component: HomeComponent
   },
 ];
 
